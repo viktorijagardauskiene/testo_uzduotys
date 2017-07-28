4e1f88ce81491335ff6fc8c46ea508f64eb05224
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-28 19:32:51 +0300

    5 uzduotis

---
+++ 5uzduotis.php
@@ -0,0 +1,5 @@
+<?php
+
+$automobilis = ["marke"=>"Ford", "modelis"=>"kazkoks", "metai"=>"2007", "spalva"=>"mėlyna", "galia"=>"labai galingas"];
+
+echo "Automobilis yra ".$automobilis["marke"]." ".$automobilis["modelis"].". Pagamintas ".$automobilis["metai"]." metais, jo spalva".$automobilis["spalva"].", ir ".$automobilis["galia"];
---
+++ 6uzduotis.php
@@ -0,0 +1,6 @@
+<?php
+
+function suma($a, $b, $c) {
+
+
+}
