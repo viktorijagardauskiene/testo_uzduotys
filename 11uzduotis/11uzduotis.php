72494ef3585049bad75013b394fc28ef946e9001
Ignas <ignas@centrofm.lt>
2017-07-30 16:06:25 +0300

    komentarai

--- 11uzduotis/11uzduotis.php
+++ 11uzduotis/11uzduotis.php
@@ -1,15 +1,32 @@
-<?php
+<pre><?php
 
 session_start();
+
+# patikrinam ka turim sesijoje
+
 print_r($_SESSION);
 
-if (isset($_POST['pavadinimas'] && $_POST['kodas']) && $_POST['pavadinimas'] !=null && $_POST['pavadinimas'] !=null) {
+# patikrinam ka gaunam is $_POST
+
+print_r($POST)
+
+
+
+
+//if (isset($_POST['pavadinimas'] && $_POST['kodas']) && $_POST['pavadinimas'] !=null && $_POST['pavadinimas'] !=null) {
 	
-} else {
-	echo 'Error';
-} 
+//} else {
+//	echo 'Error';
+//} 
 
 
+# patikrinam ar yra $_POST['submit'] arba ar yra kuris nors parametras, pvz $_POST['kodas']
+
+
+# irasom $_POST reiksmes (masyva?) i pvz $_SESSION['imone']
+
+
+# nukreipiam atgal i forma.html puslapi
 
 
 
